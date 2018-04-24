16ec7cc2d3e3869b59fbc90aa4331255c0c4c556
viklazaro <viklazaro@gmail.com>
2018-04-24 14:46:27 +0200

    Añadida la escena "EndGame", pero no funciona

--- practica3.js
+++ practica3.js
@@ -13,6 +13,19 @@ window.addEventListener("load",function() {
         .enableSound();
     // Load and init audio files.
 
+    Q.scene("endGame", function(stage){
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+        }));
+        var button = container.insert(new Q.UI.Button({x: 0, y: 0, fill: "#CCCCCC", label: "Play Again"}))
+        var label = container.insert(new Q.UI.Text({x: 10, y: -10 - button.p.h, label: stage.options.label}));
+
+        button.on("click", function(){
+            Q.clearStages();
+            Q.stageScene("level1");
+        })
+    })
+
     Q.scene("titleScreen",function(stage) {
             var container = stage.insert(new Q.UI.Container({x: Q.width / 2, y: Q.height / 2, fill: "rgba(0,0,0,0.5)"}));
             var button = container.insert(new Q.UI.Button({
@@ -169,7 +182,6 @@ window.addEventListener("load",function() {
             this.dead = true;
             this.play('marioDead');
             this.p.vy = -300;
-
         },
         /*destroyM: function(){
             this.destroy();
@@ -419,6 +431,7 @@ window.addEventListener("load",function() {
             hit: function(col) {
                 if(col.obj.isA("Player")) {
                     //Q.stageScene("endGame",1, { label: "You Died" });
+                    Q.stageScene("endGame", 2);
                     col.obj.die();
                     //Q.audio.play('hit.mp3');
                 }
